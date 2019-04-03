77a29811288a8faa0df44eb5eafb0597d5a0fde6
Paul Ajayi <pauloludele@yahoo.com>
2019-04-03 15:47:33 +0100

    initial commit

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArraySorting1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string[] fruits = new string[]
+            {
+                "cherry","orange","apple"
+            };
+            // Array.Sort(fruits);
+            var res = from fruitNames in fruits
+                      orderby fruitNames descending
+                      select fruitNames;
+
+            foreach(string fruit in res)
+            {
+                Console.WriteLine(fruit);
+                
+            }
+            Console.ReadLine();
+        }
+    }
+}
